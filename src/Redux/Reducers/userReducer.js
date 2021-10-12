968f3448946a44322e0bff786230b3fe906484cc
andrewjspivey <12spivey@gmail.com>
2021-10-12 13:48:17 -0700

    witches userReducer to switch case

--- src/Redux/Reducers/userReducer.js
+++ src/Redux/Reducers/userReducer.js
@@ -1,15 +1,16 @@
 const initialState = {};
 
 const userReducer = (state = initialState, action) => {
-  if (action.type === "LOG_IN")
-    return {
-      ...state,
-      [action.payload.target.name]: action.payload.target.value,
-    };
-  else if (action.type === "LOG_OUT") {
-    return { username: "", password: "" };
-  } else {
-    return state;
+  switch (action.type) {
+    case "LOG_IN":
+      return {
+        ...state,
+        [action.payload.target.name]: action.payload.target.value,
+      };
+    case "LOG_OUT":
+      return { username: "", password: "" };
+    default:
+      return state;
   }
 };
 
